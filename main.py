fe185c02123ffaa8bbb3a20ffe897dec04dfaa12
nWaldi <ppmip2@gmail.com>
2024-10-08 20:19:57 +0200

    read function refactor and words counting func added

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+def read_whole_book(path):
+    with open(path, 'r') as f:
+        return f.read()
+
+def count_words(book):
+    words_list = book.split()
+    return len(words_list)
+
 def main():
-    with open("books/frankenstein.txt", "r") as f:
-        print(f.read())
+    frankie = read_whole_book("books/frankenstein.txt")
+    print(frankie)
+    frankie_words_count = count_words(frankie)
+    print(frankie_words_count)
 
 
 if __name__ == "__main__":
